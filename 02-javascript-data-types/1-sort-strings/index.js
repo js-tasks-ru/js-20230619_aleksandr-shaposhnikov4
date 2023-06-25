a07214c5dd614030dd2c0581236f9fe3fffc40a4
Shaposhnikov Aleksandr <aleksandr.shaposhnikov@indusoft.ru>
2023-06-25 22:56:56 +0300

    exercise (sortStrings): done

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,19 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let arrToSort = [...arr];
+    arrToSort.sort(compareFunc);
 
+  if (param === 'desc')
+  arrToSort.reverse();
+
+  return arrToSort;
+}
+
+function compareFunc(a, b) {
+  if (a.toLowerCase() === b.toLowerCase()) {
+    return a.localeCompare(b, 'ru', {caseFirst: "upper"});
+    } else {
+    return a.localeCompare(b, 'ru', {sensitivity: 'base'});
+    }
 }
