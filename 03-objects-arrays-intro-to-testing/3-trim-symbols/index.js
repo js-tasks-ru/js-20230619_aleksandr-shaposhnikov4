055a108ef64546147935447b6e7c4c4430b8f030
Shaposhnikov Aleksandr <aleksandr.shaposhnikov@indusoft.ru>
2023-06-28 22:38:11 +0300

    exercise 03 (trimSymbols): done

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,9 +1,36 @@
-/**
+/**limit
  * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
- * @param {string} string - the initial string
+ * @param {string} string - the initial stringing
  * @param {number} size - the allowed size of consecutive identical symbols
- * @returns {string} - the new string without extra symbols according passed size
+ * @returns {string} - the new stringing without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (size === undefined) {
+    return string;
+  }
+    
+  let result = '';
+  let count = 0;
+  let prevChar = '';
+  
+  for (const char of string) {
+    
+    if (char !== prevChar) {
+      count = 1;
+      result += char;
+    } else {
+      count++;
+      if (count <= size) {
+        result += char;
+      }
+    }
+  
+    prevChar = char;
+  }
+  
+  return result;
 }
