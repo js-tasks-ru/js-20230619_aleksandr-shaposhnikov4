66eceda25eb1b79fe77a4e18c9d187ae6813b9b0
Shaposhnikov Aleksandr <aleksandr.shaposhnikov@indusoft.ru>
2023-07-07 20:01:37 +0300

    exercise 05 (notification): done

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
+    static instance = null;
+    
+    constructor(message, {duration, type} = {}) {
+      this._message = message ?? '';
+      this.duration = duration ?? 1000;
+      this._type = type ?? '';
 
+      this.render();
+    }
+    
+    render() {
+      const parent = document.createElement('div');
+      parent.innerHTML = this.getTemplate();
+      this.element = parent.firstElementChild;
+      
+      const notificationBody = this.element.querySelector('.notification-body');
+      notificationBody.textContent = this._message;
+    }
+
+    show(parentElement = document.body) {
+      if (NotificationMessage.instance) { 
+        NotificationMessage.instance.remove();
+      }
+
+      parentElement.append(this.element);
+      
+      this.timerShowId = setTimeout(() => {
+        this.destroy();
+      }, this.duration);
+      
+      NotificationMessage.instance = this;
+    }
+
+    remove() {
+      clearTimeout(this.timerShowId);
+      if (this.element) {
+        this.element.remove();
+      }
+    }
+  
+    destroy() {
+      this.remove();
+      this.element = null;
+      NotificationMessage.instance = null;
+    }
+  
+    getTemplate() {
+      const typeClass = this._type ? ` ${this._type}` : '';
+        
+      return `<div class="notification${typeClass}" style="--value:${this.duration / 1000 + "s"}">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+            <div class="notification-header">${this._type}</div>
+            <div class="notification-body">
+              ${this._message}
+            </div>
+          </div>
+        </div>`;
+    }
 }
